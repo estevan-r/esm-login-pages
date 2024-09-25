94a702d1aef66dc6b32d390000d8317e1d2f2769
estevan-r <ruiz.estevan@gmail.com>
2024-09-25 16:55:08 -0600

    Add animations to icons

--- src/components/CardIcons.jsx
+++ src/components/CardIcons.jsx
@@ -1,23 +1,29 @@
-import { Card } from "@/components/ui/card";
-import { Newspaper, OctagonAlert, Siren } from "lucide-react";
+import { Newspaper, Siren, BadgeInfo, BadgeCheck } from "lucide-react";
 
 export function NewsIcon() {
   return (
     <div className="flex isolate">
-      <div className="mb-4 grid place-items-center bg-gradient-to-b from-blue-950/75 to-blue-950/95 size-12 rounded-xl relative left-2.5 top-1.5 -rotate-6 shadow-lg group-hover:-translate-x-5 group-hover:-rotate-12 group-hover:-translate-y-0.5 transition duration-500 group-hover:duration-200">
+      <div className="mb-4 grid place-items-center bg-gradient-to-b from-blue-950/80 to-blue-900 size-12 rounded-xl relative top-0.5 -rotate-6 shadow-lg group-hover:-translate-x-2 group-hover:-rotate-12 transition duration-500 group-hover:duration-200">
         <Newspaper color="#ffd100" strokeWidth={1.5} />
       </div>
-      <div className="mb-4 grid place-items-center bg-gradient-to-b from-blue-950/75 to-blue-950/95 size-12 rounded-xl relative right-2.5 top-1.5 rotate-6 shadow-lg group-hover:translate-x-5 group-hover:rotate-12 group-hover:-translate-y-0.5 transition duration-500 group-hover:duration-200">
+      <div className="mb-4 grid place-items-center bg-gradient-to-b from-blue-950/80 to-blue-900 size-12 rounded-xl relative right-2 top-0.5 rotate-6 shadow-lg group-hover:translate-x-2 group-hover:rotate-12 transition duration-500 group-hover:duration-200">
         <Siren color="#ffd100" strokeWidth={1.5} />
       </div>
     </div>
   );
 }
 
-export function NewsIcon2() {
+export function CreateIcon() {
   return (
-    <div className="mb-4 grid place-items-center bg-gradient-to-b from-blue-950/75 to-blue-950/95 size-12 rounded-xl relative shadow-lg hover:-translate-y-1 transition duration-300">
-      <Newspaper color="#ffd100" strokeWidth={1.5} />
+    <div className="flex isolate">
+      <div className="mb-4 grid place-items-center overflow-hidden bg-gradient-to-b from-blue-950/80 to-blue-900 size-12 rounded-xl relative shadow-lg">
+        <div className="group-hover:-translate-y-6 group-hover:scale-50 group-hover:opacity-0 transition duration-500 group-hover:duration-200">
+          <BadgeInfo color="#ffd100" strokeWidth={1.5} />
+        </div>
+        <div className="absolute opacity-0 translate-y-6 scale-50 group-hover:-translate-y-0 group-hover:scale-100 group-hover:opacity-100 transition duration-500 group-hover:duration-200">
+          <BadgeCheck color="#ffd100" strokeWidth={1.5} />
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,43 +1,68 @@
 import { Card, CardHeader, CardContent, CardTitle } from "@/components/ui/card";
+import { buttonVariants } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
-import { NewsIcon } from "@/components/CardIcons";
+import { NewsIcon, CreateIcon } from "@/components/CardIcons";
+import { ArrowRight } from "lucide-react";
 
 export function Content() {
   return (
-    <div className="mt-4 grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-6 lg:grid-cols-5 xl:grid-cols-5">
-      <div className="grid auto-rows-max items-start gap-4 md:gap-6 lg:col-span-3">
-        <Card className="group">
-          <CardHeader>
-            <NewsIcon />
-            <CardTitle>News and Alerts</CardTitle>
-          </CardHeader>
-          <CardContent>
-            All employers must set up multi-factor authentication (MFA) when
-            logging into their MyUI Employer+ account. You can receive temporary
-            security codes by text message, email, or an authenticator app on
-            your phone. For more information, visit the{" "}
-            <a
-              href="https://cdle.colorado.gov/multi-factor-authentication-mfa"
-              className="underline"
-            >
-              Multi-Factor Authentication page
-            </a>{" "}
-            on our website.
-          </CardContent>
-        </Card>
-        <Card>
-          <CardHeader>
-            <CardTitle>Existing UI Accounts</CardTitle>
-          </CardHeader>
-          <CardContent>
-            If you are trying to establish yourself as a new user for an
-            existing business—a business that&apos;s already registered and has
-            a UI employer account—you must contact the system administrator for
-            your business account.
-          </CardContent>
-        </Card>
-      </div>
-      <Card className="h-full lg:col-span-2">
+    <div className="mt-4 grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-6 lg:grid-cols-2">
+      <Card className="h-full group">
+        <CardHeader>
+          <CreateIcon />
+          <CardTitle>Need an Account?</CardTitle>
+        </CardHeader>
+        <CardContent>
+          If you are an employer with an active UI account, and have not yet
+          activated your MyUI Employer+ account with a mailed code, emailed
+          link, or otherwise; or need to register a business for a UI account:
+          <a
+            className={`${buttonVariants({
+              variant: "outline",
+            })} mt-4 shadow-sm group/btn hover:bg-blue-100 transition duration-500 hover:duration-200`}
+            href="#"
+          >
+            Create an Employer User Account &nbsp;
+            <div className="relative group-hover/btn:translate-x-1 transition duration-500 group-hover/btn:duration-200">
+              <ArrowRight size={16} color="#3d3d3d" />
+            </div>
+          </a>
+        </CardContent>
+      </Card>
+
+      <Card className="h-full group">
+        <CardHeader>
+          <NewsIcon />
+          <CardTitle>News and Alerts</CardTitle>
+        </CardHeader>
+        <CardContent>
+          All employers must set up multi-factor authentication (MFA) when
+          logging into their MyUI Employer+ account. You can receive temporary
+          security codes by text message, email, or an authenticator app on your
+          phone. For more information, visit the{" "}
+          <a
+            href="https://cdle.colorado.gov/multi-factor-authentication-mfa"
+            className="underline"
+          >
+            Multi-Factor Authentication page
+          </a>{" "}
+          on our website.
+        </CardContent>
+      </Card>
+
+      <Card>
+        <CardHeader>
+          <CardTitle>Existing UI Accounts</CardTitle>
+        </CardHeader>
+        <CardContent>
+          If you are trying to establish yourself as a new user for an existing
+          business—a business that&apos;s already registered and has a UI
+          employer account—you must contact the system administrator for your
+          business account.
+        </CardContent>
+      </Card>
+
+      <Card>
         <CardHeader>
           <CardTitle>Protect Yourself from Scams</CardTitle>
         </CardHeader>
@@ -51,7 +76,7 @@ export function Content() {
                 Remember
               </Badge>
             </CardHeader>
-            <CardContent className="p-4 pt-3 text-sm text-muted-foreground">
+            <CardContent className="p-4 pt-3 text-muted-foreground">
               CDLE will never send you a text message with a link to log into
               your account. To ensure you access the official portal and not a
               fake site, access your MyUI Employer+ account via CDLE's official
